798797d0d0db8f7fe1b0c159882ce199515ebddc
Joseph Pasag <jesusfreak.13091@gmail.com>
2021-03-09 22:37:15 -0800

    Created C++ solution for project, with comment block for description.

---
+++ group1_RegistrationProject/group1_RegistrationProject/main.cpp
@@ -0,0 +1,16 @@
+/*
+Name		: Grant Dunford, Taylor McWilliam, Joseph Pasag, Terrance Usher
+Class		: CS-340
+Term		: Spring 2021
+File		: main.cpp
+Description	: Main file for the Class Registration Project for Software Engineering Methods and Project 1.
+*/
+
+
+/* Forward Declarations */
+
+int main(int argc, const char* argv[]) {
+
+
+	return 0;
+}			// end of int main(int argc, const char* argv[])
---
+++ group1_RegistrationProject/group1_RegistrationProject/temp.h
@@ -0,0 +1,11 @@
+/*
+Name		: Joseph Pasag
+Class		: CS-295
+Term		: Spring 2020
+File		:
+Description	:
+*/
+
+#include <iostream>
+
+using namespace std;
